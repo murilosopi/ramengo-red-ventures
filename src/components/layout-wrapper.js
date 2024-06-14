ae649ff739104d0917f7e66dec547303332d446d
murilosopi <dev.murilosopi@gmail.com>
2024-06-14 13:54:37 -0300

    import styles for cover section into LayoutWrapper component

--- src/components/layout-wrapper.js
+++ src/components/layout-wrapper.js
@@ -1,3 +1,5 @@
+import '../styles/layout/section-cover.scss';
+
 export function LayoutWrapper (lClass, tag = 'div') {
     const wrapper = document.createElement(tag);
     wrapper.className = lClass;
