f6203578caf05e2f4b795d13c4e43535e8323a1b
murilosopi <dev.murilosopi@gmail.com>
2024-06-14 18:51:05 -0300

    create http utils to access api endpoints

---
+++ src/utils/http.js
@@ -0,0 +1,39 @@
+const url = 'https://api.tech.redventures.com.br';
+
+const init = (method) => ({
+    headers: new Headers({
+        'Content-Type': 'application/json',
+        'x-api-key': 'ZtVdh8XQ2U8pWI2gmZ7f796Vh8GllXoN7mr0djNf'
+    }),
+    mode: 'cors',
+    method
+})
+
+const http = {
+    async get(endpoint, params) {
+        endpoint = this.treatEndpoint(endpoint) + new URLSearchParams(params).toString();
+
+        return fetch(url + endpoint, init('get')).then(this.treatResponse);
+    },
+    async post(endpoint, params) {
+        endpoint = this.treatEndpoint(endpoint);
+
+        return fetch(url + endpoint, init('post', params)).then(this.treatResponse);
+    },
+
+    treatEndpoint(endpoint) {
+        return endpoint.charAt(0) == '/' ? endpoint : '/' + endpoint;
+    },
+
+    async treatResponse(response) {
+        try {
+            return response.json();
+        } catch (e) {
+            console.warn(e);
+
+            return false;
+        }
+    }
+};
+
+export default http;
