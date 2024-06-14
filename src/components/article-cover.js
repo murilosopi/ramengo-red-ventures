652a43975903fe5577baf641509a11917a0ae925
murilosopi <dev.murilosopi@gmail.com>
2024-06-14 13:56:13 -0300

    add order button to ArticleCover component

--- src/components/article-cover.js
+++ src/components/article-cover.js
@@ -1,5 +1,7 @@
 import '../styles/components/article-cover.scss';
-import { StrongTitle } from './strong-title';
+
+import { ClickButton } from './click-button';
+import { StrongTitle } from './strong-title';                
 
 export function ArticleCover() {
     const titleContent = `<span class="u-text-vertical u-text-accent column" lang="ja">
@@ -13,10 +15,11 @@ export function ArticleCover() {
     const paragraph = document.createElement('p');
     paragraph.textContent = 'Enjoy a good ramen in the comfort of your house. Create your own ramen and choose your favorite flavour combination!';
 
+    const button = ClickButton('Order now');
 
     const article = document.createElement('article');
     article.className = 'c-article-cover u-text-light';
-    article.append(title, paragraph);
+    article.append(title, paragraph, button);
 
     return article;
 }
