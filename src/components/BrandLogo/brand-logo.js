4893d6a2f11e8a7e21fbff70a545a84413c7b6d0
murilosopi <dev.murilosopi@gmail.com>
2024-06-14 00:01:49 -0300

    create BrandLogo component

---
+++ src/components/BrandLogo/brand-logo.js
@@ -0,0 +1,12 @@
+import '../../styles/components/brand-logo.scss';
+import source from '/images/logo.svg';
+
+export function BrandLogo() {    
+    const logo = new Image();
+
+    logo.className = 'c-brand-logo';
+    logo.alt = 'ramenGO!';
+    logo.src = source;
+
+    return logo;
+}
