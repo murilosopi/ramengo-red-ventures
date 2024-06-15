2108f2a849d53a906728995b6891715701ac7f43
murilosopi <dev.murilosopi@gmail.com>
2024-06-15 01:09:58 -0300

    add layout to list of options

--- src/components/layout-wrapper.js
+++ src/components/layout-wrapper.js
@@ -1,4 +1,6 @@
 import '../styles/layout/section-cover.scss';
+import '../styles/layout/order-customizer.scss';
+import '../styles/layout/options-list.scss';
 
 export function LayoutWrapper (lClass, tag = 'div') {
     const wrapper = document.createElement(tag);
