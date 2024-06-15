2ed5534c2eecbba6b0eee00912cfd5a646a9a51b
murilosopi <dev.murilosopi@gmail.com>
2024-06-15 01:10:26 -0300

    append section order to app

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import { MainCover } from "./components/main-cover";
 import { LayoutWrapper } from './components/layout-wrapper';
+import { MainCover } from "./components/main-cover";
+import { OrderCustomizer } from './components/order-customizer';
 
 const mountComponents = () => {
     const app = document.querySelector('#app');
 
-    
     const sectionCover = LayoutWrapper('l-section-cover');
     sectionCover.appendChild(MainCover());
 
-    app.appendChild(sectionCover);
+    const sectionOrder = LayoutWrapper('l-section-order');
+    sectionOrder.appendChild(OrderCustomizer());
+
+    app.append(sectionCover, sectionOrder);
 };
 
 document.addEventListener('DOMContentLoaded', mountComponents);
