4a1eb61171fe2d496efda56dfccf069754b0b074
murilosopi <dev.murilosopi@gmail.com>
2024-06-16 01:20:24 -0300

    create SuccessContainer component

---
+++ src/components/success-container.js
@@ -0,0 +1,64 @@
+import '../styles/components/success-container.scss';
+import iconSource from '/images/bowing.svg';
+import { BrandLogo } from './brand-logo';
+import { StrongTitle } from './strong-title';
+import { ClickButton } from './click-button';
+
+export function SuccessContainer(orderResult) {    
+    const alternativeBrand = BrandLogo(true);
+
+    // Result rámen image
+    const imageResult = new Image();
+    imageResult.src = orderResult.image;
+    imageResult.alt = `${orderResult.description} image`;
+    
+    // Result section heading
+    const resultHeading = document.createElement('h1');
+    resultHeading.textContent = 'Your Order:';
+    resultHeading.className = 'heading';
+
+    // Rámen name title
+    const ramenTitle = StrongTitle(orderResult.description, 'h2');
+    ramenTitle.classList.add('u-text-accent');
+    
+    // Result content section
+    const result = document.createElement('section');
+    result.className = 'result';
+    result.append(alternativeBrand, imageResult, resultHeading, ramenTitle);
+    
+    // Order section heading
+    const orderHeading = document.createElement('h1');
+    orderHeading.textContent = 'どもありがとうございます。';
+    orderHeading.classList.add('heading', 'u-text-accent');
+
+    // Order section icon
+    const orderIcon = new Image();
+    orderIcon.src = iconSource;
+    orderIcon.alt = `Icon bowing`;
+    orderIcon.className = 'icon'
+
+    const orderText = document.createElement('p');
+    orderText.textContent = 'Hold on, when you least expect you will be eating your rámen. ';
+    orderText.className = 'text';
+
+    const orderStrongText = StrongTitle('Your order is being prepared', 'p');
+    orderStrongText.classList.add('u-text-secondary')
+
+    const buttonNewOrder = ClickButton('Place new order', 'primary');
+    
+    // Order content section
+    const order = document.createElement('section');
+    order.className = 'order';
+    order.append(orderIcon, orderHeading, orderStrongText, orderText, buttonNewOrder) 
+    
+    const container = document.createElement('main');
+    container.className = 'c-success-feedback';
+    container.append(result, order);
+
+    const handleNewOrder = () => {
+        container.dispatchEvent(new CustomEvent('new-order'));
+    };
+    buttonNewOrder.addEventListener('click', handleNewOrder);
+
+    return container;
+}
