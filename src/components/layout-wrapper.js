e2315ed24141ac2cdd4bf6c4b39d4630a2bb0094
murilosopi <dev.murilosopi@gmail.com>
2024-06-16 01:22:35 -0300

    add section success layout styles

--- src/components/layout-wrapper.js
+++ src/components/layout-wrapper.js
@@ -1,6 +1,7 @@
 import '../styles/layout/section-cover.scss';
 import '../styles/layout/order-customizer.scss';
 import '../styles/layout/options-list.scss';
+import '../styles/layout/success-section.scss';
 
 export function LayoutWrapper (lClass, tag = 'div') {
     const wrapper = document.createElement(tag);
