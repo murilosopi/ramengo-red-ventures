2ffefd677c247a78d6d3d8c8d67dd09dbe4497cb
murilosopi <dev.murilosopi@gmail.com>
2024-06-16 01:19:53 -0300

    add alternative option for BrandLogo component

--- src/components/brand-logo.js
+++ src/components/brand-logo.js
@@ -1,12 +1,13 @@
 import '../styles/components/brand-logo.scss';
 import source from '/images/logo.svg';
+import sourceAlt from '/images/logo-alt.svg';
 
-export function BrandLogo() {    
+export function BrandLogo(alt = false) {    
     const logo = new Image();
 
     logo.className = 'c-brand-logo';
     logo.alt = 'ramenGO!';
-    logo.src = source;
+    logo.src = alt ? sourceAlt : source;
 
     return logo;
 }
